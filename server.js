2d074e2fd531568198caa64def67de3079c03fe9
Aleksandra Sokolova <aleksandra.sokolova@student.lab.fi>
2025-10-03 17:32:18 +0300

    server added

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+// GET endpoint
+app.get('/api/sensor', (req, res) => {
+    res.json({
+        temperature: 22.5,
+        humidity: 55,
+        status: "OK"
+    });
+});
+
+// POST endpoint for Pico W
+app.post('/api/data', (req, res) => {
+  const { temperature, humidity } = req.body;
+  console.log('Data:', temperature, humidity);
+  // to save data
+  res.json({ status: 'OK' });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
